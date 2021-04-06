68e02fac786033ff05a14c4a355e2cbf07655d2e
Aleksei Kurilkin <kurilkin.a@yandex.ru>
2021-04-07 00:36:22 +0300

    add common_elements.py node

--- Model/common_elements.py
+++ Model/common_elements.py
@@ -9,3 +9,23 @@ class CommonBranch:
         self.ip = ip
         self.iq = iq
         self.np = np
+
+
+class CommonNode:
+    """Узел, который будет извлекаться из Растра (или Экселя)"""
+    def __init__(self, ny=0, name="", uhom=0.0,
+                 nsx=0, na=0, pn=0.0, qn=0.0,
+                 pg=0.0, qg=0.0, vzd=0.0,
+                 qmin=0.0, qmax=0.0):
+        self.ny = ny
+        self.name = name
+        self.uhom = uhom
+        self.nsx = nsx
+        self.na = na
+        self.pn = pn
+        self.qn = qn
+        self.pg = pg
+        self.qg = qg
+        self.vzd = vzd
+        self.qmin = qmin
+        self.qmax = qmax
